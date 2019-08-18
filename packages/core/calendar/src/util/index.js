0c568689d01edf670d52a36f6df07604783da9ef
Chris Morgan <cmorgan@atlassian.com>
2019-08-19 09:30:13 +1000

    [J20-1228] Improve comments about Date choices

--- packages/core/calendar/src/util/index.js
+++ packages/core/calendar/src/util/index.js
@@ -85,16 +85,18 @@ export const createLocalizationProvider = (
     minute: 'numeric',
   });
 
-  // Date range chosen which has a Mon-Sun range so we can pull the titles out
   const daysShort = [1, 2, 3, 4, 5, 6, 7].map(day =>
     // Some short days are longer than 3 characters but are unique if the first
     // three non-white characters are used.
     dayFormatter
+      // Date range chosen which has a Mon-Sun range so we can pull the titles out
       .format(new Date(2000, 4, day))
       .replace(/\s/g, '')
       .substring(0, 3),
   );
+
   const monthsLong = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(month =>
+    // Date chosen for no real reason, the only important part is the month
     monthFormatter.format(new Date(2000, month, 1)),
   );
 
