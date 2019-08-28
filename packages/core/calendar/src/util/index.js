641ff02cc14270cc2776f791e0c9557efea771ed
Chris Morgan <cmorgan@atlassian.com>
2019-08-28 18:12:28 +1000

    [J20-1228] Move LocalizationProvider to it's own file

--- packages/core/calendar/src/util/index.js
+++ packages/core/calendar/src/util/index.js
@@ -2,6 +2,11 @@
 
 import type { DateObj } from '../types';
 
+export {
+  createLocalizationProvider,
+  LocalizationProvider,
+} from './localization-provider';
+
 type DateToStringOptions = {
   fixMonth: boolean,
 };
@@ -18,53 +23,3 @@ export function dateToString(
     date.day,
   )}`;
 }
-
-export interface LocalizationProvider {
-  getDaysShort: () => Array<string>;
-  getMonthsLong: () => Array<string>;
-  formatDate: Date => string;
-  formatTime: Date => string;
-}
-
-export const createLocalizationProvider = (
-  locale: string,
-): LocalizationProvider => {
-  // Intl.DateFormat expects locales in the format of 'la-CO' however it is
-  // common for locale to be provided in the format of 'la_CO', where 'la' is
-  // language and 'CO' is country.
-  const normalizedLocale = locale && locale.replace('_', '-');
-
-  const dayFormatter = Intl.DateTimeFormat(normalizedLocale, {
-    weekday: 'short',
-  });
-  const monthFormatter = Intl.DateTimeFormat(normalizedLocale, {
-    month: 'long',
-  });
-  const dateFormatter = Intl.DateTimeFormat(normalizedLocale);
-  const timeFormatter = Intl.DateTimeFormat(normalizedLocale, {
-    hour: 'numeric',
-    minute: 'numeric',
-  });
-
-  const daysShort = [1, 2, 3, 4, 5, 6, 7].map(day =>
-    // Some short days are longer than 3 characters but are unique if the first
-    // three non-white characters are used.
-    dayFormatter
-      // Date range chosen which has a Mon-Sun range so we can pull the titles out
-      .format(new Date(2000, 4, day))
-      .replace(/\s/g, '')
-      .substring(0, 3),
-  );
-
-  const monthsLong = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11].map(month =>
-    // Date chosen for no real reason, the only important part is the month
-    monthFormatter.format(new Date(2000, month, 1)),
-  );
-
-  return {
-    getDaysShort: () => daysShort,
-    getMonthsLong: () => monthsLong,
-    formatDate: (date: Date) => dateFormatter.format(date),
-    formatTime: (date: Date) => timeFormatter.format(date),
-  };
-};
