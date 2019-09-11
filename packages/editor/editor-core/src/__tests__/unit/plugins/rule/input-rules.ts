97f4d9dfda2ecf9588a18600581c36aa46ea9561
Matias Corbanini <mcorbanini@atlassian.com>
2019-09-11 13:50:35 +1000

    ED-7503: fix test when inserting divider with `---` or `***`

--- packages/editor/editor-core/src/__tests__/unit/plugins/rule/input-rules.ts
+++ packages/editor/editor-core/src/__tests__/unit/plugins/rule/input-rules.ts
@@ -46,7 +46,7 @@ describe('inputrules', () => {
 
       insertText(editorView, '---', sel);
 
-      expect(editorView.state.doc).toEqualDocument(doc(hr(), p()));
+      expect(editorView.state.doc).toEqualDocument(doc(hr()));
       expect(trackEvent).toHaveBeenCalledWith(
         'atlassian.editor.format.horizontalrule.autoformatting',
       );
@@ -110,7 +110,7 @@ describe('inputrules', () => {
 
       insertText(editorView, '***', sel);
 
-      expect(editorView.state.doc).toEqualDocument(doc(hr(), p()));
+      expect(editorView.state.doc).toEqualDocument(doc(hr()));
       expect(trackEvent).toHaveBeenCalledWith(
         'atlassian.editor.format.horizontalrule.autoformatting',
       );
