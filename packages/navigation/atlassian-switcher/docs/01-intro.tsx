64b0f155c3aec0785e7d354b189f6d42e3d575b7
Anna Zhirnova <azhirnova@atlassian.com>
2019-09-13 10:41:42 +1000

    FIND-135: Address feedback

--- packages/navigation/atlassian-switcher/docs/01-intro.tsx
+++ packages/navigation/atlassian-switcher/docs/01-intro.tsx
@@ -16,21 +16,14 @@ export default md`
   * Their recently viewed containers, if applicable
   * Any cross-flow and admin links, if applicable
   * Any custom links from Jira or Confluence, if applicable
-
-  ## Requirements
-  
-  In order to integrate the switcher your app requires the following:
-
-  * **AtlassianID integration** - AtlassiaID is required to fetch available products. See implementation details [here](https://hello.atlassian.net/wiki/spaces/PEP/pages/494890457/1.+Sign+Up+Log+In+Anywhere).
-  * **Stargate integration** - switcher requires /gateway to be exposed through Stargate in fetch available products. See implementation details [here](https://hello.atlassian.net/wiki/spaces/PEP/pages/494890457/1.+Sign+Up+Log+In+Anywhere).
   
   ## Integrating switcher
 
   There are multiple ways to integrate switcher within your app. Depending on your application you might need to combine some of the options below.
 
   * [Atlassian switcher vanilla](/packages/navigation/atlassian-switcher-vanilla) can be used in applications that don't bundle React.
-  * [Standalone switcher](/packages/navigation/atlassian-switcher/docs/standalone-switcher) allows to render the siwtcher in any container (e.g. inline dialog) other than the drawer by specifying the appearance property.
-  * [Custom themes](/packages/navigation/atlassian-switcher/docs/theming-guide) allow to change the colours and spacing in the switcher component.
+  * [Standalone switcher](/packages/navigation/atlassian-switcher/docs/standalone-switcher) allows to render the switcher in any container (e.g. inline dialog) other than the drawer by specifying the appearance property.
+  * [Custom themes](/packages/navigation/atlassian-switcher/docs/theming-guide) allow to change the colours in the switcher component.
   
   ## Basic example
 
@@ -53,20 +46,6 @@ export default md`
     <Props
       heading="Props"
       props={require('!!extract-react-types-loader!../src/components/atlassian-switcher')}
-      overrides={{
-        // @ts-ignore
-        features: props => {
-          return null;
-        },
-        // @ts-ignore
-        messages: props => {
-          return null;
-        },
-        // @ts-ignore
-        product: props => {
-          return null;
-        },
-      }}
     />
   )}
 `;
