7233b7e3d8b9912bb6ea3af07219c4b2d7fbbcdf
Michael Blaszczyk <mblaszczyk@atlassian.com>
2019-09-16 13:49:32 +1000

    Fix mountWithIntlContext ts errors via explicit typing

--- packages/media/media-test-helpers/src/mountWithIntlContext.tsx
+++ packages/media/media-test-helpers/src/mountWithIntlContext.tsx
@@ -1,17 +1,17 @@
 import { IntlProvider, intlShape } from 'react-intl';
 import { mount, ReactWrapper } from 'enzyme';
-import { ReactElement } from 'react';
+import { Component, ReactElement } from 'react';
 
-/* TODO: Fix enzyme type resolution error
- * We need to type this to any so a d.ts file is created since typescript is currently
- * erroring out with
+/* TODO: We are explicitly using the third arg of ReactWrapper to work around the following TS issue which prevents a d.ts from being generated
+ * and therefore fails the build:
  * error TS2742: The inferred type of 'mountWithIntlContext' cannot be named without a reference to 'react-transition-group/node_modules/@types/react'. This is likely not portable. A type annotation is necessary.
+ * TS is resolving enzyme's usage of react to react-transition-group???
  */
-export const mountWithIntlContext: any = <P, S>(
+export const mountWithIntlContext = <P, S>(
   node: ReactElement<P>,
   reactContext?: Object,
   childContextTypes?: Object,
-): ReactWrapper<P, S> => {
+): ReactWrapper<P, S, Component<P, S>> => {
   const intlProvider = new IntlProvider({
     locale: 'en',
     messages: {},
@@ -21,5 +21,5 @@ export const mountWithIntlContext: any = <P, S>(
   return mount<P, S>(node, {
     context: { intl, ...reactContext },
     childContextTypes: { intl: intlShape, ...childContextTypes },
-  });
+  }) as ReactWrapper<P, S, Component<P, S>>;
 };
