149b2bda266eb93bed4c4276743c5562818717a7
Alex Hixon <ahixon@atlassian.com>
2019-09-16 12:39:35 -0700

    add nestable_taskList to validator

--- packages/editor/adf-utils/src/validator/specs/nestableTaskList.ts
+++ packages/editor/adf-utils/src/validator/specs/nestableTaskList.ts
@@ -1,6 +1,10 @@
 export default {
   props: {
-    content: { type: 'array', items: ['taskItem'], minItems: 1 },
+    content: {
+      type: 'array',
+      items: ['taskItem', ['taskItem', 'nestableTaskList']],
+      minItems: 1,
+    },
     type: { type: 'enum', values: ['taskList'] },
     attrs: { props: { localId: { type: 'string' } } },
   },
