182eca6193a27cd68c51b63207a8960e4d142881
Michael Blaszczyk <mblaszczyk@atlassian.com>
2019-09-16 13:20:24 +1000

    Suppress d.ts generation error in mountWithIntlContext

--- packages/media/media-test-helpers/src/mountWithIntlContext.tsx
+++ packages/media/media-test-helpers/src/mountWithIntlContext.tsx
@@ -2,7 +2,12 @@ import { IntlProvider, intlShape } from 'react-intl';
 import { mount, ReactWrapper } from 'enzyme';
 import { ReactElement } from 'react';
 
-export const mountWithIntlContext = <P, S>(
+/* TODO: Fix enzyme type resolution error
+ * We need to type this to any so a d.ts file is created since typescript is currently
+ * erroring out with
+ * error TS2742: The inferred type of 'mountWithIntlContext' cannot be named without a reference to 'react-transition-group/node_modules/@types/react'. This is likely not portable. A type annotation is necessary.
+ */
+export const mountWithIntlContext: any = <P, S>(
   node: ReactElement<P>,
   reactContext?: Object,
   childContextTypes?: Object,
