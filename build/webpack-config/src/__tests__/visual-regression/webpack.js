48fb6d88004d5980228c75e4cab25bb7fd6a2055
raja <rbellebon@atlassian.com>
2019-09-17 10:21:42 +1000

    Improve webpack and dynamic table

--- build/webpack-config/src/__tests__/visual-regression/webpack.js
+++ build/webpack-config/src/__tests__/visual-regression/webpack.js
@@ -2,7 +2,7 @@
 import { takeElementScreenShot } from '@atlaskit/visual-regression/helper';
 
 const app = '#app';
-const atlaskitTitle = 'h1';
+const atlaskitTitle = 'h1[data-testid="title"]';
 
 describe('Webpack Website Snapshot >', () => {
   it('Home page title should match production', async () => {
@@ -10,7 +10,7 @@ describe('Webpack Website Snapshot >', () => {
     const { page } = global;
     await page.goto(url, { waitUntil: 'networkidle0' });
     await page.waitForSelector(app);
-    await page.setViewport({ width: 1800, height: 1000 });
+    await page.setViewport({ width: 1240, height: 1000 });
     await page.waitForSelector(atlaskitTitle);
     const image = await takeElementScreenShot(page, atlaskitTitle);
     // $FlowFixMe
