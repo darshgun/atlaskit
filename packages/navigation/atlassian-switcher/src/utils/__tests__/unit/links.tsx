39b080a94cf4d5db31aa8d57b5b06b20c032d1a4
Jimmy Kurniawan <jkurniawan@atlassian.com>
2019-09-19 11:13:40 +1000

    FIND-336 Fixed test

--- packages/navigation/atlassian-switcher/src/utils/__tests__/unit/links.tsx
+++ packages/navigation/atlassian-switcher/src/utils/__tests__/unit/links.tsx
@@ -51,13 +51,19 @@ describe('utils/links', () => {
     it('should have link for People', () => {
       const isDiscoverMoreForEveryoneEnabled = false;
       const expectedProducts = ['people'];
-      const fixedLinks = getFixedProductLinks(isDiscoverMoreForEveryoneEnabled);
+      const fixedLinks = getFixedProductLinks(
+        true,
+        isDiscoverMoreForEveryoneEnabled,
+      );
       expect(fixedLinks.map(({ key }) => key)).toMatchObject(expectedProducts);
     });
     it('should have discover more button if enabled', () => {
       const isDiscoverMoreForEveryoneEnabled = true;
       const expectedProducts = ['people', 'discover-more'];
-      const fixedLinks = getFixedProductLinks(isDiscoverMoreForEveryoneEnabled);
+      const fixedLinks = getFixedProductLinks(
+        true,
+        isDiscoverMoreForEveryoneEnabled,
+      );
       expect(fixedLinks.map(({ key }) => key)).toMatchObject(expectedProducts);
     });
   });
