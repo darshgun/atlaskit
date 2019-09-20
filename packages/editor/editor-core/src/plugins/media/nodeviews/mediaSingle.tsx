956452d13c958b1b23b81714b4e3632c862d3d16
Hector Leon Zarco Garcia <hezarco@gmail.com>
2019-09-20 11:43:48 +1000

    Call updateFileAttrs on prop change

--- packages/editor/editor-core/src/plugins/media/nodeviews/mediaSingle.tsx
+++ packages/editor/editor-core/src/plugins/media/nodeviews/mediaSingle.tsx
@@ -69,8 +69,9 @@ export default class MediaSingleNode extends Component<
   UNSAFE_componentWillReceiveProps(nextProps: MediaSingleNodeProps) {
     if (nextProps.mediaProvider !== this.props.mediaProvider) {
       this.setViewMediaClientConfig(nextProps);
-      this.createMediaNodeUpdater(nextProps).updateFileAttrs();
     }
+
+    this.createMediaNodeUpdater(nextProps).updateFileAttrs();
   }
 
   setViewMediaClientConfig = async (props: MediaSingleNodeProps) => {
