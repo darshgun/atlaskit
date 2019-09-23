1dcb8050e8ce10725e546d6c7d9636fdb10218f3
raja <rbellebon@atlassian.com>
2019-09-24 06:09:42 +1000

    Adding badge integration test and fix the example

--- packages/core/badge/examples/99-testing.tsx
+++ packages/core/badge/examples/99-testing.tsx
@@ -1,37 +1,33 @@
 import React from 'react';
-import styled from 'styled-components';
-import { borderRadius, colors } from '@atlaskit/theme';
 import Badge from '../src';
-
-interface ItemProps {
-  inverted?: boolean;
-}
-
-const Item = styled.div<ItemProps>`
-  align-items: center;
-  background: ${props => (props.inverted ? colors.B400 : 'none')};
-  border-radius: ${borderRadius}px;
-  color: ${props => (props.inverted ? colors.N0 : 'inherit')};
-  display: flex;
-  justify-content: space-between;
-  margin-bottom: 4px;
-  max-width: 300px;
-  padding: 0.6em 1em;
-
-  &:hover {
-    background-color: ${props => (props.inverted ? colors.B500 : colors.N20)};
-  }
-`;
 
 export default function Example() {
   return (
     <div>
-      <Item>
-        <p>Added</p>
-        <Badge appearance="added" max={99} testId="myBadge">
-          {3000}
+      <p>
+        Added
+        <Badge appearance="added" max={99} testId="myBadgeAdded">
+          {2}
         </Badge>
-      </Item>
+      </p>
+      <p>
+        default
+        <Badge appearance="default" max={99} testId="myBadgeDefault">
+          {67}
+        </Badge>
+      </p>
+      <p>
+        important
+        <Badge appearance="important" max={99} testId="myBadgeImportant">
+          {20}
+        </Badge>
+      </p>
+      <p>
+        primary
+        <Badge appearance="primary" max={99} testId="myBadgePrimary">
+          {19}
+        </Badge>
+      </p>
     </div>
   );
 }
