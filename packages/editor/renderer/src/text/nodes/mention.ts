460433e4870b1dd01a7fcf13d3e2670178e7ee93
jxie <jxie@atlassian.com>
2019-09-24 14:21:37 +1000

    CS-114 : fix up lint warning.

--- packages/editor/renderer/src/text/nodes/mention.ts
+++ packages/editor/renderer/src/text/nodes/mention.ts
@@ -2,7 +2,7 @@ import { Node as PMNode, Schema } from 'prosemirror-model';
 import { NodeReducer } from './';
 
 const mention: NodeReducer = (node: PMNode, schema: Schema) => {
-  if (['all', 'here'].indexOf(node.attrs.id) != -1) {
+  if (['all', 'here'].indexOf(node.attrs.id) !== -1) {
     return `@${node.attrs.id}`;
   }
   return `${node.attrs.text || '@unknown'}`;
