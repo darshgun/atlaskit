4fee868d209f37528f0fbab0f2d3bf1f3386fe10
jxie <jxie@atlassian.com>
2019-09-24 14:03:00 +1000

    CS-114 : use `indexOf` rather than `includes` for IE11 compatibility.

--- packages/editor/renderer/src/text/nodes/mention.ts
+++ packages/editor/renderer/src/text/nodes/mention.ts
@@ -2,7 +2,7 @@ import { Node as PMNode, Schema } from 'prosemirror-model';
 import { NodeReducer } from './';
 
 const mention: NodeReducer = (node: PMNode, schema: Schema) => {
-  if (['all', 'here'].includes(node.attrs.id)) {
+  if (['all', 'here'].indexOf(node.attrs.id) != -1) {
     return `@${node.attrs.id}`;
   }
   return `${node.attrs.text || '@unknown'}`;
