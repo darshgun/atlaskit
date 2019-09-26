cdcdc962682286695cb9db4f849a944638ab42de
Matias Corbanini <mcorbanini@atlassian.com>
2019-09-26 15:10:20 +1000

    ED-7738: add alt text editor prop

--- packages/editor/editor-core/src/editor.tsx
+++ packages/editor/editor-core/src/editor.tsx
@@ -53,6 +53,7 @@ export default class Editor extends React.Component<EditorProps, {}> {
     disabled: false,
     extensionHandlers: {},
     allowNewInsertionBehaviour: true,
+    UNSAFE_allowAltTextOnImages: false,
   };
 
   static contextTypes = {
