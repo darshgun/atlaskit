764ea118eae81289b1c519aae5dc404c37e28460
Anna Zhirnova <azhirnova@atlassian.com>
2019-09-27 11:38:46 +1000

    FIND-363: Overhaul switcher example

--- packages/navigation/atlassian-switcher/examples/01-generic-switcher.tsx
+++ packages/navigation/atlassian-switcher/examples/01-generic-switcher.tsx
@@ -1,11 +1,11 @@
 import * as React from 'react';
-import Drawer from '@atlaskit/drawer';
 import Button from '@atlaskit/button';
-import { mockEndpoints, REQUEST_MEDIUM } from './helpers/mock-endpoints';
+import Drawer from '@atlaskit/drawer';
+import { mockEndpoints } from './helpers/mock-endpoints';
 import { withAnalyticsLogger, withIntlProvider } from './helpers';
 import AtlassianSwitcher from '../src';
 
-class SwitcherExample extends React.Component {
+class GenericSwitcherExample extends React.Component {
   state = {
     isDrawerOpen: false,
   };
@@ -15,8 +15,12 @@ class SwitcherExample extends React.Component {
   }
 
   openDrawer = () => {
-    mockEndpoints('jira', originalMockData => originalMockData, REQUEST_MEDIUM);
-
+    mockEndpoints('confluence', originalMockData => originalMockData, {
+      containers: 1000,
+      xflow: 500,
+      permitted: 2000,
+      appswitcher: 1500,
+    });
     this.setState({
       isDrawerOpen: true,
     });
@@ -28,10 +32,6 @@ class SwitcherExample extends React.Component {
     });
   };
 
-  onDiscoverMoreClicked = () => {
-    console.log(`Triggering discover more!`);
-  };
-
   onTriggerXFlow = (productKey: string, sourceComponent: string) => {
     console.log(
       `Triggering xflow for => ${productKey} from ${sourceComponent}`,
@@ -39,31 +39,20 @@ class SwitcherExample extends React.Component {
   };
 
   render() {
-    const greenishColorScheme = {
-      primaryTextColor: '#006400',
-      secondaryTextColor: '#4ca64c',
-      primaryHoverBackgroundColor: '#cce5cc',
-      secondaryHoverBackgroundColor: '#e5f2e5',
-    };
-
     return (
       <div style={{ padding: '2rem' }}>
         <Drawer onClose={this.onClose} isOpen={this.state.isDrawerOpen}>
           <AtlassianSwitcher
-            product="site-admin"
+            product="generic-product"
             cloudId="some-cloud-id"
-            triggerXFlow={this.onTriggerXFlow}
-            isDiscoverMoreForEveryoneEnabled
-            onDiscoverMoreClicked={this.onDiscoverMoreClicked}
-            theme={greenishColorScheme}
           />
         </Drawer>
         <Button type="button" onClick={this.openDrawer}>
-          Open user-centric drawer
+          Open drawer
         </Button>
       </div>
     );
   }
 }
 
-export default withIntlProvider(withAnalyticsLogger(SwitcherExample));
+export default withIntlProvider(withAnalyticsLogger(GenericSwitcherExample));
