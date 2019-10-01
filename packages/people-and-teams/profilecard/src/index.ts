becd298e2ce1d141ebfac5f9644887dac9ccc902
Thorsten Basse <tbasse@atlassian.com>
2019-10-01 13:50:15 +1000

    PTC-2589: export show and hide delay times of profile card trigger

--- packages/people-and-teams/profilecard/src/index.ts
+++ packages/people-and-teams/profilecard/src/index.ts
@@ -1,11 +1,15 @@
 import ProfileCard from './components/ProfileCard';
 import ProfileCardClient, { modifyResponse } from './api/ProfileCardClient';
 import ProfileCardResourced from './components/ProfileCardResourced';
-import ProfileCardTrigger from './components/ProfileCardTrigger';
+import ProfileCardTrigger, {
+  DELAY_MS_SHOW,
+  DELAY_MS_HIDE,
+} from './components/ProfileCardTrigger';
 export * from './types';
 
 export { ProfileCard };
 export { ProfileCardTrigger };
 export { ProfileCardClient as ProfileClient, modifyResponse };
+export { DELAY_MS_SHOW, DELAY_MS_HIDE };
 
 export default ProfileCardResourced;
