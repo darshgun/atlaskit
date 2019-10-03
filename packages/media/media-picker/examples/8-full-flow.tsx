4a7b31831a414681f8ff7af15f75b777a11ca7ba
Hector Leon Zarco Garcia <hezarco@gmail.com>
2019-10-03 12:57:33 +1000

    Add event logs to full flow example

--- packages/media/media-picker/examples/8-full-flow.tsx
+++ packages/media/media-picker/examples/8-full-flow.tsx
@@ -19,6 +19,8 @@ import {
   UploadPreviewUpdateEventPayload,
   MediaFile,
   Popup,
+  UploadProcessingEventPayload,
+  UploadEndEventPayload,
 } from '../src';
 
 const userMediaClientConfig = createUploadMediaClientConfig();
@@ -81,10 +83,20 @@ export default class Example extends React.Component<{}, State> {
     });
 
     popup.on('upload-preview-update', this.onUploadPreviewUpdate);
+    popup.on('upload-processing', this.onUploadProcessing);
+    popup.on('upload-end', this.onUploadEnd);
     this.setState({ popup });
 
     popup.show();
   }
+
+  onUploadProcessing = (event: UploadProcessingEventPayload) => {
+    console.log('onUploadProcessing', event.file.id);
+  };
+
+  onUploadEnd = (event: UploadEndEventPayload) => {
+    console.log('onUploadEnd', event.file.id);
+  };
 
   private onUploadPreviewUpdate = async (
     event: UploadPreviewUpdateEventPayload,
