a5d5ef54b9265aa78a8bf8d7150e926f49e7320b
Gora Kong <gora.kong1@gmail.com>
2019-10-03 18:47:55 -0700

    oops

--- packages/editor/editor-core/src/__tests__/unit/plugins/media/nodeviews/mediaNodeUpdater.spec.ts
+++ packages/editor/editor-core/src/__tests__/unit/plugins/media/nodeviews/mediaNodeUpdater.spec.ts
@@ -32,13 +32,7 @@ describe('MediaNodeUpdater', () => {
     jest.spyOn(commands, 'replaceExternalMedia').mockReturnValue(() => true);
     jest
       .spyOn(mediaCommon, 'getViewMediaClientConfigFromMediaProvider')
-<<<<<<< HEAD
       .mockReturnValue(Promise.resolve(getDefaultMediaClientConfig()));
-=======
-      // @ts-ignore This violated type definition upgrade of @types/jest to v24.0.18 & ts-jest v24.1.0.
-      //See BUILDTOOLS-210-clean: https://bitbucket.org/atlassian/atlaskit-mk-2/pull-requests/7178/buildtools-210-clean/diff
-      .mockReturnValue(getDefaultMediaClientConfig());
->>>>>>> 9df0bd1331... typeError suppressions
 
     const contextIdentifierProvider: Promise<
       ContextIdentifierProvider
