ac3fe103b3a1d1e0d889cc1712a5daa9c0a1ca67
raja <rbellebon@atlassian.com>
2019-10-08 15:44:20 +1100

    WIP

--- packages/core/icon/src/components/__tests__/unit/server-side-hydrate.tsx
+++ packages/core/icon/src/components/__tests__/unit/server-side-hydrate.tsx
@@ -16,26 +16,28 @@ afterEach(() => {
 });
 
 test('should ssr then hydrate icon correctly', async () => {
-  const [example] = await getExamplesFor('icon');
-  const Example = await require(example.filePath).default; // eslint-disable-line import/no-dynamic-require
-
-  const elem = document.createElement('div');
-  elem.innerHTML = await ssr(example.filePath);
-
-  ReactDOM.hydrate(<Example />, elem);
-  await waitForExpect(() => {
-    // ignore warnings caused by emotion's server-side rendering approach
-    // @ts-ignore
-    // eslint-disable-next-line no-console
-    const mockCalls = console.error.mock.calls.filter(
-      // @ts-ignore
-      ([f, s]) =>
-        !(
-          f ===
-            'Warning: Did not expect server HTML to contain a <%s> in <%s>.' &&
-          s === 'style'
-        ),
-    );
-    expect(mockCalls.length).toBe(0);
-  });
+  const example = await getExamplesFor('icon');
+  console.log(example);
+  // console.log('After example', new Date().getTime())
+  // const Example = await require(example.filePath).default; // eslint-disable-line import/no-dynamic-require
+  // console.log('After require path', new Date().getTime())
+  // const elem = document.createElement('div');
+  // elem.innerHTML = await ssr(example.filePath);
+  // ReactDOM.hydrate(<Example />, elem);
+  // await waitForExpect(() => {
+  //   // ignore warnings caused by emotion's server-side rendering approach
+  //   // @ts-ignore
+  //   // eslint-disable-next-line no-console
+  //   const mockCalls = console.error.mock.calls.filter(
+  //     // @ts-ignore
+  //     ([f, s]) =>
+  //       !(
+  //         f ===
+  //           'Warning: Did not expect server HTML to contain a <%s> in <%s>.' &&
+  //         s === 'style'
+  //       ),
+  //   );
+  //   expect(mockCalls.length).toBe(0);
+  //   console.log('test finish', new Date().getTime())
+  // });
 });
