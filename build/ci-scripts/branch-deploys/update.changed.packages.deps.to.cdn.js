c038fb86449475b1897d5e07e3ead8fdb0d4c5fb
Michael Blaszczyk <mblaszczyk@atlassian.com>
2019-10-10 16:47:41 +1100

    Revert "pull request #7298 merged  by Landkid after a successful build rebased on Master"
    This reverts commit fde3583ed467891db211da33113e51fb499bbf1a, reversing
    changes made to b292e004ffe41fae4c86d580b78f2e66e1cabae2.

--- build/ci-scripts/branch-deploys/update.changed.packages.deps.to.cdn.js
+++ build/ci-scripts/branch-deploys/update.changed.packages.deps.to.cdn.js
@@ -1,12 +1,7 @@
 /**
- * This script is used during branch deploys to update all the package.json's of changed packages
+ * This script is used during branch deployss to update all the package.json's of changed packages
  * so that if they have any dependencies on other changed packages, they also point to the branch
  * deployed version.
- *
- * It also updates the version of each package.json to a unique pre-release version to prevent a yarn resolution bug where it incorrectly resolves
- * the actual version of the package and our branch deployed version to the same yarn.lock entry if they are both added at the same time as a result of
- * them both being a transitive dependency.
- * That issue seems to cause https://github.com/yarnpkg/yarn/issues/2629 & https://github.com/yarnpkg/yarn/issues/6312 as well.
  */
 
 const bolt = require('bolt');
@@ -33,10 +28,7 @@ const commit = BITBUCKET_COMMIT.substr(0, 12);
 
 const getExpectedUrl = (pkgName, pkgVersion, commit) => {
   const shortPkgName = pkgName.replace('@atlaskit/', 'atlaskit-');
-  // pkgVersion may or may not contain the commit pre-release version at this stage, so we normalise
-  const baseVersion = pkgVersion.replace(`-${commit}`, '');
-  const normalisedVersion = `${baseVersion}-${commit}`;
-  return `https://s3-ap-southeast-2.amazonaws.com/atlaskit-artefacts/${commit}/dists/${shortPkgName}-${normalisedVersion}.tgz`;
+  return `https://s3-ap-southeast-2.amazonaws.com/atlaskit-artefacts/${commit}/dists/${shortPkgName}-${pkgVersion}.tgz`;
 };
 
 bolt.getWorkspaces().then(workspaces => {
@@ -47,11 +39,8 @@ bolt.getWorkspaces().then(workspaces => {
   changedPackagesInfo.forEach(pkg => {
     const packageJsonPath = path.join(pkg.dir, 'package.json');
     const pkgJson = JSON.parse(fs.readFileSync(packageJsonPath));
-
-    pkgJson.version = `${pkg.config.version}-${commit}`;
-    console.log(`Updating version of ${pkgJson.name} to ${pkgJson.version}`);
-
-    Object.entries(pkgJson.dependencies).forEach(([depName]) => {
+    let pkgJsonDirty = false;
+    Object.entries(pkgJson.dependencies).forEach(([depName, depVersion]) => {
       let cpInfo = changedPackagesInfo.find(
         changedPkg => changedPkg.name === depName,
       );
@@ -63,11 +52,14 @@ bolt.getWorkspaces().then(workspaces => {
         console.log(
           `Updating dep of ${pkgJson.name}: ${depName} - ${expectedUrl}`,
         );
+        pkgJsonDirty = true;
       }
     });
 
-    const pkgJsonStr = JSON.stringify(pkgJson, null, 2);
-    console.log(`Updating package.json at ${packageJsonPath}`);
-    fs.writeFileSync(packageJsonPath, pkgJsonStr);
+    if (pkgJsonDirty) {
+      const pkgJsonStr = JSON.stringify(pkgJson, null, 2);
+      console.log(`Updating package.json at ${packageJsonPath}`);
+      fs.writeFileSync(packageJsonPath, pkgJsonStr);
+    }
   });
 });
