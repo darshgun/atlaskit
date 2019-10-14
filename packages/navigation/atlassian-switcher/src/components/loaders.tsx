d6b3a6b4d35bf0df9aceb0f4600e30c732f00e16
Anna Zhirnova <azhirnova@atlassian.com>
2019-10-14 13:41:54 +1100

    PTC-2647: Use kebab case for switcher chunk names

--- packages/navigation/atlassian-switcher/src/components/loaders.tsx
+++ packages/navigation/atlassian-switcher/src/components/loaders.tsx
@@ -1,16 +1,16 @@
 import Loadable from 'react-loadable';
 
 export const loadAtlassianSwitcher = () =>
-  import(/* webpackChunkName: "@atlaskit/atlassian-switcher/async-chunk/atlassian-switcher" */ './atlassian-switcher');
+  import(/* webpackChunkName: "@atlaskit-atlassian-switcher-async-chunk-atlassian-switcher" */ './atlassian-switcher');
 
 export const loadJiraSwitcher = () =>
-  import(/* webpackChunkName: "@atlaskit/atlassian-switcher/async-chunk/jira-switcher" */ './jira-switcher');
+  import(/* webpackChunkName: "@atlaskit-atlassian-switcher-async-chunk-jira-switcher" */ './jira-switcher');
 
 export const loadConfluenceSwitcher = () =>
-  import(/* webpackChunkName: "@atlaskit/atlassian-switcher/async-chunk/confluence-switcher" */ './confluence-switcher');
+  import(/* webpackChunkName: "@atlaskit-atlassian-switcher-async-chunk-confluence-switcher" */ './confluence-switcher');
 
 export const loadGenericSwitcher = () =>
-  import(/* webpackChunkName: "@atlaskit/atlassian-switcher/async-chunk/generic-switcher" */ './generic-switcher');
+  import(/* webpackChunkName: "@atlaskit-atlassian-switcher-async-chunk-generic-switcher" */ './generic-switcher');
 
 export const AtlassianSwitcherLoader = Loadable({
   loader: loadAtlassianSwitcher,
