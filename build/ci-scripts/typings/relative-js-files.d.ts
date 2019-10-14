da96fb97eb8932026a5201dccad3c3cfab132e62
Michael Blaszczyk <mblaszczyk@atlassian.com>
2019-10-14 12:37:00 +1100

    Compile js files in build tsconfig
    This means we don't need the hacky relative wildcard typing workaround
    and fixes vscode 'go to definition' for those files.

--- build/ci-scripts/typings/relative-js-files.d.ts
+++
@@ -1,3 +0,0 @@
-// Use wildcard notation to ignore implicit any errors for all untyped (js) modules imported as './foo';
-// You can add more relatives like '../*' if you come across it
-declare module './*';
