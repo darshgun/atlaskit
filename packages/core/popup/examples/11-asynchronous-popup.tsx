e9984dcc1fcc6776e6f9d4a62b076bd72544ed6a
Harsha Ivaturi <isriharsha@atlassian.com>
2019-10-17 13:38:49 +1100

    Use a static loading text instead of Spinner to make the VR tests reliable

--- packages/core/popup/examples/11-asynchronous-popup.tsx
+++ packages/core/popup/examples/11-asynchronous-popup.tsx
@@ -2,7 +2,6 @@
 import { FC, useState, useEffect } from 'react';
 import Button from '@atlaskit/button';
 import { Placement } from '@atlaskit/popper';
-import Spinner from '@atlaskit/spinner';
 import { jsx } from '@emotion/core';
 
 import Popup from '../src';
@@ -64,7 +63,7 @@ const PopupContent: FC<PopupProps> = ({
 
   return loading ? (
     <div id="spinner" css={spinnerContainerCSS}>
-      <Spinner size="large" />
+      <div>Loading...</div>
     </div>
   ) : (
     <div id="popup-content" css={contentCSS}>
