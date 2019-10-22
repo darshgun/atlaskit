9a0c8bc204379aa05be49a90dba00391bf975292
Michael Blaszczyk <mblaszczyk@atlassian.com>
2019-10-22 11:12:11 +1100

    Address PR feedback

--- jest-lint-config.js
+++ jest-lint-config.js
@@ -1,11 +1,10 @@
+/**
+ * We are using jest-runner-eslint to execute eslint rules in parallel, resulting in faster builds.
+ */
 /* eslint-disable flowtype/require-valid-file-annotation */
 const config = {
   runner: 'jest-runner-eslint',
   testMatch: [`${__dirname}/**/*.(js|tsx|ts)`],
 };
-
-if (process.env.CI) {
-  config.maxWorkers = '50%';
-}
 
 module.exports = config;
