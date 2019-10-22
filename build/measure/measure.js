98692ea2df1260f1732b4024f245bca63a698146
raja <rbellebon@atlassian.com>
2019-10-22 15:29:11 +1100

    Adding a custom build for target branch and replace master by target branch

--- build/measure/measure.js
+++ build/measure/measure.js
@@ -27,6 +27,8 @@ const {
 
 const { returnMissingPkgBasedOn } = require('./utils/error.js');
 
+const targetBranch = process.env.TARGET_BRANCH || targetBranch;
+
 function fWriteStats(path, content) {
   fs.writeFileSync(path, JSON.stringify(clearStats(content), null, 2), 'utf8');
 }
@@ -236,12 +238,12 @@ module.exports = async function main(
     fWriteStats(masterStatsFilePath, stats);
     if (process.env.CI) {
       // upload to s3 masterStats
-      uploadToS3(masterStatsFilePath, 'master');
+      uploadToS3(masterStatsFilePath, targetBranch);
     }
   } else {
     if (process.env.CI) {
       try {
-        await downloadFromS3(masterStatsFolder, 'master', packageName);
+        await downloadFromS3(masterStatsFolder, targetBranch, packageName);
       } catch (err) {
         const errorMessage = `${err}`;
         if (errorMessage.includes('not found in s3 bucket')) {
@@ -251,13 +253,17 @@ module.exports = async function main(
             `${missingPkg}-bundle-size-ratchet.json`,
           );
           fWriteStats(masterStatsFilePath, stats);
-          uploadToS3(masterStatsFilePath, 'master');
+          uploadToS3(masterStatsFilePath, targetBranch);
         } else {
           throw err;
         }
       }
     } else {
-      await downloadFromS3ForLocal(masterStatsFolder, 'master', packageName);
+      await downloadFromS3ForLocal(
+        masterStatsFolder,
+        targetBranch,
+        packageName,
+      );
     }
   }
 
