f30aa6ba27ca0740cbee87d05c7bb86b75e645d8
raja <rbellebon@atlassian.com>
2019-10-24 19:28:59 +1100

    Try to remove those jest-haste-map collision

--- jest-lint-config.js
+++ jest-lint-config.js
@@ -5,6 +5,7 @@
 const config = {
   runner: 'jest-runner-eslint',
   testMatch: ['<rootDir>/**/*.(js|tsx|ts)'],
+  modulePathIgnorePatterns: ['/__fixtures__/'],
   watchPlugins: ['jest-runner-eslint/watch-fix'],
 };
 
