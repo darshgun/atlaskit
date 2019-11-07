526b4d41bf743abbd949a8d606151b9163092c5e
Mario Nebl <mnebl@atlassian.com>
2019-11-07 16:03:38 +1100

    style: apply prettier formatting

--- packages/editor/renderer/src/__tests__/unit/ui/Renderer.tsx
+++ packages/editor/renderer/src/__tests__/unit/ui/Renderer.tsx
@@ -230,7 +230,9 @@ describe('@atlaskit/renderer/ui/Renderer', () => {
       },
     ];
     appearances.forEach(appearance => {
-      it(`adds appearance to analytics events for ${appearance.appearance} renderer`, () => {
+      it(`adds appearance to analytics events for ${
+        appearance.appearance
+      } renderer`, () => {
         renderer = initRendererWithAnalytics({
           appearance: appearance.appearance,
         });
