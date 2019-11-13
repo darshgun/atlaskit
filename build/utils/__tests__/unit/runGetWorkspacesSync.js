99280839cf26d1d7ad9a4d2326be2cedf2aaab27
Ben Conolly <ben@thinkmill.com.au>
2019-11-13 15:14:45 +1100

    should get landkid working again

--- build/utils/__tests__/unit/runGetWorkspacesSync.js
+++ build/utils/__tests__/unit/runGetWorkspacesSync.js
@@ -1,14 +1,19 @@
-const getWorkspacesSync = require('./../../getWorkspacesSync')
-  .getWorkspacesSync;
+// @flow
 const bolt = require('bolt');
 const fs = require('fs');
 const path = require('path');
+const { getWorkspacesSync } = require('./../../getWorkspacesSync');
 
 describe('getWorkspacesSync >', () => {
   test('should return the same results as getWorkspaces', async () => {
     const resultsSync = getWorkspacesSync();
     const resultsAsync = await bolt.getWorkspaces();
+
     expect(resultsSync.length).toBe(resultsAsync.length);
-    expect(resultsSync).toStrictEqual(resultsAsync);
+
+    console.log();
+
+    expect(resultsSync).toEqual(expect.arrayContaining(resultsAsync));
+    // expect(resultsSync).toStrictEqual(resultsAsync);
   });
 });
