142d05957ba336d34713a21ca0df7fc5be8ec73e
jxie <jxie@atlassian.com>
2019-11-21 18:05:09 +1100

    JSDCLOUD-8620 : add unit test for the dom pollyfill.

--- packages/editor/editor-wikimarkup-transformer/src/parser/tokenize/link-text.ts
+++ packages/editor/editor-wikimarkup-transformer/src/parser/tokenize/link-text.ts
@@ -1,4 +1,5 @@
 import { isSafeUrl } from '@atlaskit/adf-schema';
+import { decode } from '../utils/url';
 import { Token, TokenParser } from './';
 
 // https://www.atlassian.com
@@ -56,27 +57,4 @@ function fallback(input: string, position: number): Token {
     text: input.substr(position, 1),
     length: 1,
   };
-}
-
-/**
- * returns a correctly percent-encoded & sanitized url.
- * Will fallback to using a DOM based implementation if the `URL` class
- * doesn't exist.
- *
- * @param raw a 'raw' url (possibly mixed percent-encoded).
- */
-function decode(raw: string): string {
-  if (URL) {
-    const decoded = new URL(raw);
-    // IE11 doesn't support the `href` property in the `URL` class.
-    // Fallback to the DOM below if it doesn't exist.
-    if (decoded.href) {
-      return decoded.href;
-    }
-  }
-  // no `URL` class - pollyfill using the dom to parse and
-  // decode. This should only be needed for IE11 or lower...
-  const anchor = document.createElement('a');
-  anchor.href = raw; // The DOM will parses even mixed %-encoded segments of a url when set.
-  return anchor.href;
 }
