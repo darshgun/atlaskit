054af01e9a59836ef15d0c7f9b41ff9b751607ba
raja <rbellebon@atlassian.com>
2019-11-25 17:00:47 +1100

    Update tests to have more granularity

--- packages/helpers/motion/src/__tests__/visual-regression/motionTestSnapshot.ts
+++ packages/helpers/motion/src/__tests__/visual-regression/motionTestSnapshot.ts
@@ -4,11 +4,15 @@ import {
 } from '@atlaskit/visual-regression/helper';
 
 // Css-selectors
-const menu = "[data-testid='menu']";
 const examples = '#examples';
+const menu = "[data-testid='menu']";
+const button1 = "[data-testid='button--1']";
+const button2 = "[data-testid='button--2']";
+const button3 = "[data-testid='button--3']";
+const button4 = "[data-testid='button--4']";
+const button5 = "[data-testid='button--5']";
 
-// TODO: Rework the tests
-describe.skip('Snapshot Test', () => {
+describe('Snapshot Test', () => {
   let page: any;
   let url: string;
 
@@ -25,17 +29,63 @@ describe.skip('Snapshot Test', () => {
     await page.goto(url);
   });
 
-  it('Motion example resizing height should match production example when adding a new element', async () => {
+  it('Motion example resizing height should match production example when adding 1 element', async () => {
     await page.waitForSelector(examples);
-    for (let i = 1; i <= 5; i++) {
-      const button = `[data-testid="button--${i}"]`;
-      await page.waitForSelector(button);
-      await page.click(button);
-      // The motionanimation takes 0.2s
-      await page.waitFor(500);
-      // @ts-ignore custom properties on global are untyped
-      const image = await takeElementScreenShot(page, menu);
-      expect(image).toMatchProdImageSnapshot();
-    }
+    await page.waitForSelector(button1);
+    await page.click(button1);
+    await page.waitForSelector(menu);
+    // The motionanimation takes 0.2s but due to the rendering it takes more time to load.
+    await page.waitFor(500);
+    // @ts-ignore custom properties on global are untyped
+    const image = await takeElementScreenShot(page, menu);
+    expect(image).toMatchProdImageSnapshot();
+  });
+
+  it('Motion example resizing height should match production example when adding 2 elements', async () => {
+    await page.waitForSelector(examples);
+    await page.waitForSelector(button2);
+    await page.click(button2);
+    await page.waitForSelector(menu);
+    // The motionanimation takes 0.2s but due to the rendering it takes more time to load.
+    await page.waitFor(500);
+    // @ts-ignore custom properties on global are untyped
+    const image = await takeElementScreenShot(page, menu);
+    expect(image).toMatchProdImageSnapshot();
+  });
+
+  it('Motion example resizing height should match production example when adding 3 elements', async () => {
+    await page.waitForSelector(examples);
+    await page.waitForSelector(button3);
+    await page.click(button3);
+    await page.waitForSelector(menu);
+    // The motionanimation takes 0.2s but due to the rendering it takes more time to load.
+    await page.waitFor(500);
+    // @ts-ignore custom properties on global are untyped
+    const image = await takeElementScreenShot(page, menu);
+    expect(image).toMatchProdImageSnapshot();
+  });
+
+  it('Motion example resizing height should match production example when adding 4 elements', async () => {
+    await page.waitForSelector(examples);
+    await page.waitForSelector(button4);
+    await page.click(button4);
+    await page.waitForSelector(menu);
+    // The motionanimation takes 0.2s but due to the rendering it takes more time to load more elements.
+    await page.waitFor(800);
+    // @ts-ignore custom properties on global are untyped
+    const image = await takeElementScreenShot(page, menu);
+    expect(image).toMatchProdImageSnapshot();
+  });
+
+  it('Motion example resizing height should match production example when adding 5 elements', async () => {
+    await page.waitForSelector(examples);
+    await page.waitForSelector(button5);
+    await page.click(button5);
+    await page.waitForSelector(menu);
+    // The motionanimation takes 0.2s but due to the rendering it takes more time to more elements.
+    await page.waitFor(800);
+    // @ts-ignore custom properties on global are untyped
+    const image = await takeElementScreenShot(page, menu);
+    expect(image).toMatchProdImageSnapshot();
   });
 });
