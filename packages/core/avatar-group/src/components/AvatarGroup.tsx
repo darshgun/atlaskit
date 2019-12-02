b32c68b2c0d7647df35f36599b10cb79ee136aa8
Michael Dougall <mdougall@atlassian.com>
2019-12-03 09:33:02 +1100

    fix(types): replaces use of unknown with appropriate equivalents

--- packages/core/avatar-group/src/components/AvatarGroup.tsx
+++ packages/core/avatar-group/src/components/AvatarGroup.tsx
@@ -42,7 +42,7 @@ interface Props {
   onAvatarClick?: AvatarClickType;
   /** Take control of the click event on the more indicator. This will cancel
    the default dropdown behavior. */
-  onMoreClick?: (event: React.MouseEvent) => unknown;
+  onMoreClick?: (event: React.MouseEvent) => void;
   /** Provide additional props to the MoreButton. Example use cases: altering
    tab order by providing tabIndex; adding onClick behaviour without losing the
    default dropdown */
