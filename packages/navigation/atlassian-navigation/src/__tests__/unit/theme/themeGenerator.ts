c2b866b0c5e42759230c5594df9478ed1cc40c9c
Harsha Ivaturi <isriharsha@atlassian.com>
2019-12-05 19:48:42 +1100

    Remove console statement

--- packages/navigation/atlassian-navigation/src/__tests__/unit/theme/themeGenerator.ts
+++ packages/navigation/atlassian-navigation/src/__tests__/unit/theme/themeGenerator.ts
@@ -21,9 +21,6 @@ describe('generateTheme', () => {
       Object.keys(theme).forEach(component => {
         it(`should match theme object for "${component}"`, () => {
           const componentTheme = theme[component as Component];
-          if (component == 'productHome') {
-            console.log(componentTheme);
-          }
           expect(Object.keys(componentTheme)).toEqual(
             Object.keys(themes[i].mode[component as Component]),
           );
