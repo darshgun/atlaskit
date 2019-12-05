932f49bf022b897d3a8d3f31a5303f311f89ea22
Vijay Sutrave <vsutrave@atlassian.com>
2019-12-05 16:22:48 +1100

    Adding changelog functions

---
+++ .changeset/changelogFunctions.js
@@ -0,0 +1,39 @@
+const { BITBUCKET_REPO_FULL_NAME } = process.env;
+
+if (!BITBUCKET_REPO_FULL_NAME) {
+  throw Error('$BITBUCKET_REPO_FULL_NAME environment variable needs to be set');
+}
+
+const getLinkMD = commit => {
+  return `[${commit}](https://bitbucket.org/${BITBUCKET_REPO_FULL_NAME}/commits/${commit})`;
+};
+
+const getReleaseLine = async (changeset, versionType) => {
+  const indentedSummary = changeset.summary
+    .split('\n')
+    .map(l => `  ${l}`.trimRight())
+    .join('\n');
+
+  return `- [${versionType}] ${getLinkMD(
+    changeset.commit,
+  )}:\n\n${indentedSummary}`;
+};
+
+const getDependencyReleaseLine = async (changesets, dependenciesUpdated) => {
+  if (dependenciesUpdated.length === 0) return '';
+
+  const changesetLinks = changesets.map(
+    changeset => `- Updated dependencies ${getLinkMD(changeset.commit)}:`,
+  );
+
+  const updatedDepenenciesList = dependenciesUpdated.map(
+    dependency => `  - ${dependency.name}@${dependency.newVersion}`,
+  );
+
+  return [...changesetLinks, ...updatedDepenenciesList].join('\n');
+};
+
+module.exports = {
+  getReleaseLine,
+  getDependencyReleaseLine,
+};
