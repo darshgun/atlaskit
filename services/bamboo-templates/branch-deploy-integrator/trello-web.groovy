81bbd2eb117534ebb97917786673421b141069b1
Marco De Jongh <mdejongh@atlassian.com>
2019-12-19 16:13:20 +1100

    add trello

---
+++ services/bamboo-templates/branch-deploy-integrator/trello-web.groovy
@@ -0,0 +1,18 @@
+plan(key:'ABDITR',name:'trello Atlaskit Branch Deploy Integrator',
+    description:'Creates branches on trello pulling in Atlaskit branch deploys to give Atlaskit build results on their PRs. **NOTE**: Master is disabled but every other branch is enabled',
+    enabled:'false') {
+    createBranchDeployIntegrator(sourceRepo: 'Atlaskit-MK-2', productRepo: 'trello-web', productCiPlanUrl: '')
+    branchMonitoring() {
+        createBranch(matchingPattern:'risky-.*|develop|release-candidate.*')
+        inactiveBranchCleanup(periodInDays:'14')
+        deletedBranchCleanup(periodInDays:'7')
+    }
+    dependencies(triggerForBranches:'true')
+    planMiscellaneous() {
+        hungBuildKiller(enabled:'true')
+        planOwnership(owner:'mdejongh')
+    }
+    permissions() {
+        user(name:'mdejongh',permissions:'read,write,build,clone,administration')
+    }
+}
