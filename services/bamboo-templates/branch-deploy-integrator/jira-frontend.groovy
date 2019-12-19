37acecdd3b4051c84f87a27b1231a7218a78ff10
Marco De Jongh <mdejongh@atlassian.com>
2019-12-19 14:15:20 +1100

    Jira-frontend and empty readme

---
+++ services/bamboo-templates/branch-deploy-integrator/jira-frontend.groovy
@@ -0,0 +1,18 @@
+plan(key:'ABDICC',name:'jira-frontend Atlaskit Branch Deploy Integrator',
+    description:'Creates branches on jira-frontend pulling in Atlaskit branch deploys to give Atlaskit build results on their PRs. **NOTE**: Master is disabled but every other branch is enabled',
+    enabled:'false') {
+    createBranchDeployIntegrator(sourceRepo: 'Atlaskit-MK-2', productRepo: 'jira-frontend')
+    branchMonitoring() {
+        createBranch(matchingPattern:'risky-.*|develop|release-candidate.*')
+        inactiveBranchCleanup(periodInDays:'14')
+        deletedBranchCleanup(periodInDays:'7')
+    }
+    dependencies(triggerForBranches:'true')
+    planMiscellaneous() {
+        hungBuildKiller(enabled:'true')
+        planOwnership(owner:'mdejongh')
+    }
+    permissions() {
+        user(name:'mdejongh',permissions:'read,write,build,clone,administration')
+    }
+}
